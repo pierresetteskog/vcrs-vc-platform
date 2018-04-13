6719efb0dc2dfa441868355259f0f82beba7ae68
Pierre setteskog <pierre.setteskog@wipcore.se>
2018-04-13 14:56:30 +0200

    using miss

--- VirtoCommerce.Platform.Web/Swagger/SwaggerConfig.cs
+++ VirtoCommerce.Platform.Web/Swagger/SwaggerConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Web.Hosting;
 using System.Web.Http;
